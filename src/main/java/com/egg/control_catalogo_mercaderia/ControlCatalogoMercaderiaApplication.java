20ac5ee6462e3d6cf1e9d7df259eab7f759784fe
Melissa Diaz <120139230+melodiaz23@users.noreply.github.com>
2025-03-17 09:18:13 -0500

    initialize project with Spring Boot and Maven wrapper

---
+++ src/main/java/com/egg/control_catalogo_mercaderia/ControlCatalogoMercaderiaApplication.java
@@ -0,0 +1,13 @@
+package com.egg.control_catalogo_mercaderia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlCatalogoMercaderiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlCatalogoMercaderiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/egg/control_catalogo_mercaderia/ControlCatalogoMercaderiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egg.control_catalogo_mercaderia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlCatalogoMercaderiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
