21b6568b209bf22a0677423b33e599203e3f872f
Melissa Diaz <120139230+melodiaz23@users.noreply.github.com>
2025-03-17 19:09:09 -0500

    Creación de entidades Artículo, Fabrica, Imagen, y Usuario

---
+++ src/main/java/com/egg/catalogo_mercaderia/entity/Articulo.java
@@ -0,0 +1,40 @@
+package com.egg.catalogo_mercaderia.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "articulos")
+public class Articulo {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id_articulo")
+  private Long idArticulo;
+
+  @Column(name = "nro_articulo", unique = true, nullable = false)
+  private Integer nroArticulo;
+
+  @Column(name = "nombre_articulo", length = 100, nullable = false)
+  private String nombreArticulo;
+
+  @Column(name = "descripcion_articulo", length = 255, nullable = false)
+  private String descripcionArticulo;
+
+  @ManyToOne
+  @JoinColumn(name = "id_fabrica")
+  private Fabrica fabrica;
+
+  @OneToMany(mappedBy = "articulo", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+  private List<Imagen> imagenList = new ArrayList<>();
+
+
+}
